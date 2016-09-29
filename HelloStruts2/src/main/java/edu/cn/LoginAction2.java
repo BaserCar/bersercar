d21415719ff46119a1299e970703359def382664
BaserCar <a928628198@163.com>
2016-09-29 10:19:23 +0800

    作业

---
+++ HelloStruts2/src/main/java/edu/cn/LoginAction2.java
@@ -0,0 +1,29 @@
+package edu.cn;
+public class LoginAction2 {
+
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String execute(){
+		if ("admin".equals(username) && "123456".equals(password)) {  
+            return "success";  
+        } else {  
+            return "login";  
+        }  
+		
+		
+		
+	}
+}
